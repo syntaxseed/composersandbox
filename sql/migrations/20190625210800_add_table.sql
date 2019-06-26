2b27035d0fca7b00f6b18ef80db2ba7c03f4d6c3
Sherri Wheeler <825423+syntaxseed@users.noreply.github.com>
2019-06-26 14:44:14 -0400

    Add SQL migrations.

--- sql/migrations/20190625210800_add_table.sql
+++ sql/migrations/20190625210800_add_table.sql
@@ -5,17 +5,8 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
---
--- Database: `avinus_db1_composersandbox`
---
 
--- --------------------------------------------------------
-
---
--- Table structure for table `syntaxseed_iplimiter`
---
-
-CREATE TABLE IF NOT EXISTS `syntaxseed_thisisatest` (
+CREATE TABLE IF NOT EXISTS `migration_test` (
   `ip` varchar(39) CHARACTER SET ascii NOT NULL,
   `category` varchar(128) NOT NULL,
   `attempts` int(10) UNSIGNED NOT NULL DEFAULT '1',
@@ -23,4 +14,9 @@ CREATE TABLE IF NOT EXISTS `syntaxseed_thisisatest` (
   `banned` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`ip`,`category`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+
+
 COMMIT;
