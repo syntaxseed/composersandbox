69f394314f04f110681ea6e65d0b8f555956ec27
Syntaxseed <825423+syntaxseed@users.noreply.github.com>
2022-04-08 13:12:00 -0400

    Replace phue package with my fork.

--- public/hue.php
+++ public/hue.php
@@ -1,8 +1,12 @@
 <?php
 use \Phue\Client;
-use \Phue\Command\Ping;
+use \Phue\Command\CreateScene;
+use \Phue\Command\DeleteScene;
 use \Phue\Command\GetLights;
+use \Phue\Command\Ping;
+use \Phue\Command\SetGroupState;
 use \Phue\Command\SetLightState;
+use \Phue\Command\SetSceneLightState;
 use Phue\Transport\Exception\ConnectionException;
 
 require_once '../vendor/autoload.php';
@@ -30,43 +34,84 @@ try {
 
     $lights = $client->getLights();
     $groups = $client->getGroups();
+    $scenes = $client->getScenes();
 
-    /*
-    foreach ($lights as $lightId => $light) {
-        echo "Id #{$lightId} - {$light->getName()}<br>";
-    }
-    */
+    echo('<h2>Initial Scene Set:</h2>');
+    //displayScenes($client);
+
+    // Create a scene.
+
+    $lights[1]->setOn(true);
+    $lights[1]->setRGB(0, 0, 255);
+    $lights[2]->setOn(true);
+    $lights[2]->setRGB(0, 0, 255);
+    sleep(1);
+
+
+
+    $client->sendCommand(new CreateScene('phparch', 'PHP Architect', [1, 2]));
+
+    $command = new SetSceneLightState('phparch', 1);
+    $command = $command->brightness(200)
+        ->rgb(220, 90, 33); // Orange
+    $client->sendCommand($command);
+
+    $command = new SetSceneLightState('phparch', 2);
+    $command = $command->brightness(200)
+        ->rgb(60, 70, 175);  // Purple
+    $client->sendCommand($command);
+
 
     echo('<hr>');
+    echo('<h2>Updated Scene Set:</h2>');
+    $scenes = displayScenes($client);
+
+    print_r($scenes['phparch']);
+
+    echo('<hr>');
+    echo('Turning on scene...<br>');
+
+    $lights[1]->setOn(true);
+    $lights[1]->setRGB(0, 255, 0);
+    $lights[2]->setOn(true);
+    $lights[2]->setRGB(0, 255, 0);
+    sleep(3);
+
+    $command = new SetGroupState($groups[2]);
+    $command->scene('phparch');
+    $client->sendCommand($command);
+
+    sleep(5);
+    echo('Deleting scene...<br>');
+
+    $scenes['phparch']->delete();
+
+    echo('<h2>Deleted Scene Set:</h2>');
+    displayScenes($client);
+
+    exit();
+
+
+
+
+    $lights[1]->setRGB(0, 255, 0);
+    $lights[2]->setRGB(0, 255, 0);
+
+    sleep(1);
+
+
+
+
     /*
-    foreach ($groups as $group) {
-        $groupLightIds = implode(', ', $group->getLightIds());
-        echo <<<EOT
-            Id #{$group->getId()} - {$group->getName()}
-            (Type: {$group->getType()})
-            Lights: {$groupLightIds}<br>
-        EOT;
+    for ($i = 1; $i <= 5; $i++) {
+        $light->setRGB(0, 0, 255);
+        sleep(2); // Wait for 2 seconds.
+        $light->setRGB(0, 255, 0);
+        sleep(2); // Wait for 2 seconds.
+        $light->setRGB(155, 0, 0);
+        sleep(2); // Wait for 2 seconds.
     }
     */
-
-    /*
-    Id #1 - Hue color lamp 1
-    Id #2 - Hue color lamp 2
-    Id #3 - Hue color -Sherri's bedside
-    Id #4 - Hue color - Kevin's bedside
-    Id #1 - Master Bedroom (Type: Room) Lights: 4, 3
-    Id #2 - Family Room (Type: Room) Lights: 1, 2
-    */
-
-
-
-    $light = $lights[2];
-
-    $light->setOn(false);
-
-    sleep(2); // Wait for 2 seconds.
-    $light->setOn(true);
-    $light->setBrightness(100);
 
     // $light->setColorTemp(153);
     // sleep(2); // Wait for 2 seconds.
@@ -89,6 +134,7 @@ try {
 
 
     // Flickering candle effect.
+    /*
     for ($i = 1; $i < 100; $i++) {
         // Randomly choose values.
         $brightness = rand(20, 50);
@@ -108,6 +154,66 @@ try {
         // Sleep for transition time plus some extra for request time.
         usleep($waitTime * 1000000 + 25000);
     }
+    */
 } catch (ConnectionException $e) {
     echo 'ERROR: There was a problem accessing the Hue bridge.';
+}
+
+
+/*
+Light Id #1 - Hue color lamp 1
+Light Id #2 - Hue color lamp 2
+Light Id #3 - Hue color -Sherri's bedside
+Light Id #4 - Hue color - Kevin's bedside
+*/
+function displayLights($client)
+{
+    $lights = $client->getLights();
+    foreach ($lights as $lightId => $light) {
+        echo "Light Id #{$lightId} - {$light->getName()}<br>";
+    }
+    return $lights;
+}
+
+/*
+Group Id #1 - Master Bedroom (Type: Room) Lights: 4, 3
+Group Id #2 - Family Room (Type: Room) Lights: 1, 2
+Group Id #200 - Living room music area (Type: Entertainment) Lights: 1, 2
+Group Id #201 - Bedroom music area (Type: Entertainment) Lights: 4, 3
+*/
+function displayGroups($client)
+{
+    $groups = $client->getGroups();
+    foreach ($groups as $group) {
+        $groupLightIds = implode(', ', $group->getLightIds());
+        echo <<<EOT
+            Group Id #{$group->getId()} - {$group->getName()}
+            (Type: {$group->getType()})
+            Lights: {$groupLightIds}<br>
+        EOT;
+    }
+    return $groups;
+}
+
+/*
+Scene Id #qJbs3HT4XQaatx2 - Savanna sunset (Lights: 3, 4)
+Scene Id #O0cshlfoi1LNeys - Tropical twilight (Lights: 3, 4)
+Scene Id #u7Bk9fBJ7qtiRnA - Arctic aurora (Lights: 3, 4)
+Scene Id #JVz9N-5hvIs-BbC - Spring blossom (Lights: 3, 4)
+Scene Id #Ri1gzX2-KYpczwE - Spring blossom (Lights: 1, 2)
+Scene Id #RQDUc6yccQ8kav1 - Relax (Lights: 1, 2)
+Scene Id #zahvZZ30xwtRTPQ - Read (Lights: 1, 2)
+...
+*/
+function displayScenes($client)
+{
+    $scenes = $client->getScenes();
+    foreach ($scenes as $scene) {
+        $lightIds = implode(', ', $scene->getLightIds());
+        echo <<<EOT
+            Scene Id #{$scene->getId()} - {$scene->getName()}
+            (Lights: {$lightIds})<br>
+        EOT;
+    }
+    return $scenes;
 }
